8a3196dcf8621d85832e4840b4e06f06c4af7acb
Diego Soria Rios <diegosoriarios@MacBook-Pro-de-AdminAurum.local>
2021-08-15 20:21:58 -0300

    New features

---
+++ js/story.js
@@ -0,0 +1,9 @@
+const action = 0
+const actions = [
+  [0, "Welcome", FILE_TYPES.TEXT, "Welcome to 13OS;You only have 13kb of storage space, please be careful what you store, but also be careful what you delete, you can't restore;Follow all the rules and the country will be happy;A list of rules will be sent at you every day;THE ADVISOR", 25, null],
+  [1, "Rules", FILE_TYPES.TEXT, "The following words can't be read at the goverment;Blue;Room;Sad", 6, null],
+  [2, "Wonderful", FILE_TYPES.TEXT, "I see trees of green, red roses too;I see them bloom for me and you;And I think to myself: What a wonderful world;I see skies of blue and clouds of white;The bright blessed days...", 18, null],
+  [3, "Banker", FILE_TYPES.TEXT, "A banker is a fellow who lends you his umbrella when the sun is shining, but wants it back the minute it begins to rain", 12, null],
+  [4, "Dogs", FILE_TYPES.TEXT, "We could have bought a small yacht with what we spent on our dog an dall the things he destroyed. Then again, how many yachts wait by the door all day for your return?", 16, null],
+  
+]
